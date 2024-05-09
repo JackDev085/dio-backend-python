bba4f204d568644d1215163f59abb4b9d2baa37f
Jackson Lorran <computadordb@gmail.com>
2024-05-08 21:06:33 -0300

    database

---
+++ py_database/iindex.py
@@ -0,0 +1,7 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+conn = sqlite3.connect(ROOT_PATH/"clientes.sqlite")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE clientes(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(240), email VARCHAR(240))")
